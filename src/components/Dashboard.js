c7f61451f5e0ace71dd8f2f724463bf92170fa87
Katie K <katiekucsera@gmail.com>
2024-01-11 17:58:27 -0500

    Add state and function to handle focused panel

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -30,18 +30,28 @@ const data = [
 class Dashboard extends Component {
   state = {
     loading: false,
+    focused: null
   };
 
+  selectPanel(id) {
+    this.setState(previousState => ({
+      focused: previousState.focused !== null ? null : id
+    }));
+  }
+
   render() {
-    const dashboardClasses = classnames("dashboard");
+    const dashboardClasses = classnames("dashboard", {
+      "dashboard--focused": this.state.focused
+    });
 
     if (this.state.loading) {
       return <Loading />;
     }
 
-    const panels = data.map(({ id, label, value }) => (
-      <Panel key={id} id={id} label={label} value={value} />
-    ));
+    const panels = (this.state.focused ? data.filter(panel => this.state.focused === panel.id) : data)
+      .map(({ id, label, value }) => (
+        <Panel key={id} label={label} value={value} onSelect={event => this.selectPanel(id)} />
+      ));
 
     return <main className={dashboardClasses}>{panels}</main>;
 
